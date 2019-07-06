c488fe76f106d13046078414c64991aa9fcd10c2
Vitalii Vitaminenko <vitamin@ukr.net>
2019-07-06 17:01:33 +0300

    Updates

---
+++ precache-manifest.14d70311141648b728f0e5bced4b561c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74c7a26df3f49478508c0b3793f9216",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "4285023e1a67846f5eb3",
+    "url": "/repository-name/static/css/main.183f50a3.chunk.css"
+  },
+  {
+    "revision": "99614aac79a90dfcec4c",
+    "url": "/repository-name/static/js/2.e4443d61.chunk.js"
+  },
+  {
+    "revision": "4285023e1a67846f5eb3",
+    "url": "/repository-name/static/js/main.d4cdb031.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  }
+]);
