cc4bfbad3d11a3d45d5eab21c87fa332e15387b0
Vitalii Vitaminenko <vitamin@ukr.net>
2019-07-06 16:46:27 +0300

    add localstorage and pagination

--- HW11/src/App.js
+++ HW11/src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import Search from "./Search";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
+import Repo from "./Repo";
 
 function App() {
   return (
-    <Container>
+    <Container style={{ padding: 50 }}>
       <Router>
         <Switch>
-          <Route path="/" component={Search} />
+          <Route path="/repo/:id" component={Repo} />
+          <Route exact path="/" component={Search} />
         </Switch>
       </Router>
     </Container>
